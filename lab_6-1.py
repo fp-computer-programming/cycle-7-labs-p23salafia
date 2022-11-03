a24c60fd7f8164ef58224d8232d0438ea4bfc662
Jan <p23jsalafia@fairfieldprep.org>
2022-11-03 10:58:35 -0400

    6-1 Lab Submission

---
+++ lab_6-1.py
@@ -0,0 +1,12 @@
+# JS Lab 6 11/3/2022
+#Define Variable for my list
+my_arr = [1, 2, 3, 4, 5]
+#Print the number 3, which is in the 2nd index
+print(my_arr[2])
+#Find the length of my list
+print(len(my_arr))
+#Print my list 3 times
+print([1, 2, 3, 4, 5]*3)
+#Converting a string into a list
+s1 = 'Fairfield'
+print(list(s1))
