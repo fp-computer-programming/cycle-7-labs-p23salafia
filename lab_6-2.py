d346edf204f094b4d25b4f4ec4a23cfaa8c53a2d
Jan <p23jsalafia@fairfieldprep.org>
2022-11-04 11:16:29 -0400

    6-2 Lab Submission

---
+++ lab_6-2.py
@@ -0,0 +1,14 @@
+#JS Lab 6-2 11/4/2022
+#Assigning variable to my list
+my_row = ['Tanner', 'Nathaniel']
+my_row[1] = 'Jan'                       #Replace Last index with my name
+print(my_row)
+
+#Creating a clone of list
+my_row2 = my_row
+print(my_row2)
+
+#Statement that removes index value 1
+del my_row[1]
+print(my_row)
+#The last index value is removed
