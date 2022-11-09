e18b7731b84ed50e8f455b21492bc69738492075
Jan <p23jsalafia@fairfieldprep.org>
2022-11-09 15:02:00 -0500

    6-5 Lab Submission

--- lab_6-5.py
+++ lab_6-5.py
@@ -1,13 +1,15 @@
 #JS Lab 6-5 11/9/2022
 #Make a list of numbers
-numbers = [30,10,20]
-#statements to sort #s
-numbers.sort(reverse=True)
-#Conditional to make sure there are at least 2 numbers
-if len(numbers) < numbers[2]:
-    print("Error: List does not meet specifications")
-#Contitional to make sure there are at least 2 unique numbers
-if numbers[0]==numbers[1]:
-    print("Error: List does not meet specifications")
+numbers = ["30","10","40"]
 
-print()
+#Conditional to make sure there are at least 2 numbers
+#Sort the list
+#Print the highest and lowest number
+if len(numbers)< 2:
+    print("Error: List does not meet specifications")
+else:
+    numbers.sort(reverse=True)
+    print(numbers[0] + " is the highest number")
+    print(numbers[-1] + " is the lowest number")
+
+
